666f8d304c1b3b6fa38b7d59bfe6dd9526b4f3ac
Twan-Reijinga <twan.reijinga@coderclass.nl>
2023-07-13 21:48:09 +0200

    make fontsize dynamic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,9 +5,4 @@
 
 <section>
 	<TitleSubtitle title="Hello World" subtitle="I'm Twan" />
-	<!-- <div class="titleBox">
-		<h1>Hello World!</h1>
-	</div>
-
-	<h2>I'm Twan</h2> -->
 </section>
--- src/routes/TitleSubtitle.svelte
+++ src/routes/TitleSubtitle.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
+	import { onMount, afterUpdate } from 'svelte';
 	import './textStyles.css';
 	export let title: string;
 	export let subtitle: string;
+
+	let fontSize: string;
+
+	onMount(() => {
+		updateFontSize();
+		window.addEventListener('resize', updateFontSize);
+	});
+
+	afterUpdate(() => {
+		updateFontSize();
+	});
+
+	function updateFontSize() {
+		fontSize = window.innerWidth / 20 + 'px';
+	}
 </script>
 
 <div class="titleSubtitleBox">
 	<div class="titleBox">
-		<h1>{title}</h1>
+		<h1 style="font-size: {fontSize}">{title}</h1>
 	</div>
-	<h2>{subtitle}</h2>
+	<h2 style="font-size: {fontSize}">{subtitle}</h2>
 </div>
 
 <style>
