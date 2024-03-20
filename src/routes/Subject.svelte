a8a4044ec96f1031dc347579ab8793e5cba849e7
Twan Reijinga <43951307+Twan-Reijinga@users.noreply.github.com>
2024-03-20 08:50:08 +0100

    implement easter Egg

--- src/routes/Subject.svelte
+++ src/routes/Subject.svelte
@@ -1,21 +1,35 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
+	import '../styles/roll.css';
 	export let text: string;
 	export let image: 'programming' | 'experience';
 	let y: number;
 	let h: number;
+	let isActiveEasterEgg: boolean = false;
+	$: if (isActiveEasterEgg) document.body.classList.add('roll');
+
+	onMount(() => {
+		let easterEgg = document.querySelector('.programming');
+		easterEgg?.addEventListener('click', roll);
+	});
+
+	function roll() {
+		isActiveEasterEgg = true;
+		setTimeout(() => {
+			isActiveEasterEgg = false;
+			document.body.classList.remove('roll');
+		}, 2000);
+	}
 </script>
 
 <svelte:window bind:scrollY={y} bind:innerHeight={h} />
 <div class="horizontal" transition:fade={{ duration: 100 }}>
-	<div
-		class="imageHolder"
-	>
+	<div class="imageHolder">
 		<div class="image {image}" />
 	</div>
 	<h3>{text}</h3>
 </div>
-
 
 <style>
 	.horizontal {
@@ -29,7 +43,7 @@
 		background-color: #fff;
 		border-radius: 12%;
 		transform: rotate(45deg);
-		box-shadow: 7px -7px #000000AA;
+		box-shadow: 7px -7px #000000aa;
 	}
 	.horizontal h3 {
 		padding-left: 5%;
