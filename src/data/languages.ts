6bedf1a873ea838b39f88a84e3efb0a35ead2faf
Twan Reijinga <43951307+Twan-Reijinga@users.noreply.github.com>
2025-03-15 00:02:38 +0100

    Add java to langs

--- src/data/languages.ts
+++ src/data/languages.ts
@@ -20,7 +20,8 @@ const languages: Language[] = [
 	{ text: 'ELM', color: '#5C6278', textColor: '#fff' },
 	{ text: 'Unity', color: '#6D6D6D', textColor: '#fff' },
 	{ text: 'R', color: '#1F63B6', textColor: '#fff' },
-	{ text: 'NodeJS', color: '#6cc24a', textColor: '#000' }
+	{ text: 'NodeJS', color: '#6cc24a', textColor: '#000' },
+	{ text: 'Java'. color: '#ff9820', textColor: '#000' },
 ];
 
 export default languages;
