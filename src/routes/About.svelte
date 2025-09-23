4d1c756f0f936fa3c101e9420d15699bc26966da
Twan Reijinga Reijinga <t.b.reijinga@student.tudelft.nl>
2025-09-23 16:46:40 +0200

    update about me

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -13,8 +13,8 @@
 		<span>Programming</span> is, apart from <span>skiing</span>,<br />
 		the greatest thing there is.<br />
 		Besides Studing <span>Computer Science</span> at <span>TU Delft</span>,<br>
-		you can find me <span>rowing</span> on the Schie at <span>Proteus-Eretes</span><br />
-		or help kids make <span>robots</span> at NewTechKids.<br />
+		you can find me <span>rowing</span> on the Schie all day<br />
+		or build the new site with the <span>WebCie</span> at <span>Proteus-Eretes</span> <br />
 	</h3>
 	<Subject text="Most used languages" image="programming" />
 	<!-- {#if y > h / 1.4} -->
